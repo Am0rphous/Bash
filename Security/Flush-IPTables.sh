955a5357c43edee89fc6f80b92decef365ccf1b0
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2020-11-12 19:08:36 +0100

    Create Flush-IPTables.sh

---
+++ Security/Flush-IPTables.sh
@@ -0,0 +1,20 @@
+# Accept all traffic first to avoid ssh lockdown  via iptables firewall rules #
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+ 
+# Flush All Iptables Chains/Firewall rules #
+iptables -F
+ 
+# Delete all Iptables Chains #
+iptables -X
+ 
+# Flush all counters too #
+iptables -Z 
+# Flush and delete all nat and  mangle #
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables iptables -t raw -F
+iptables -t raw -X
