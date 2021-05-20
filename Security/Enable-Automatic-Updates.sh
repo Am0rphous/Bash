d1da8f8a745b18e28e73357265a5cffe965449d8
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2021-05-20 11:40:13 +0200

    a few better changes :)

--- Security/Enable-Automatic-Updates.sh
+++ Security/Enable-Automatic-Updates.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 
-#source: https://brainfood.xyz/post/20191116-setup-gitlab-runner-with-docker-in-docker/
+#source and inspo: https://brainfood.xyz/post/20191116-setup-gitlab-runner-with-docker-in-docker/
 
-# Install Unattended Upgrades package
-sudo apt update
-sudo apt install unattended-upgrades
+# Install Unattended-Upgrades package
+sudo apt update && sudo apt install unattended-upgrades
 
 # Configure Unattended Upgrades
 cat << EOF | sudo tee /etc/apt/apt.conf.d/50unattended-upgrades
-# Enable Upgrade for all package types
+// documentation: https://wiki.debian.org/UnattendedUpgrades
+// Enable Upgrade for all package types
 Unattended-Upgrade::Allowed-Origins {
         "${distro_id}:${distro_codename}";
         "${distro_id}:${distro_codename}-security";
         "${distro_id}ESM:${distro_codename}";
+        "${distro_id}ESM:${distro_codename}-infra-security";
         "${distro_id}:${distro_codename}-updates";
         "${distro_id}:${distro_codename}-proposed";
         "${distro_id}:${distro_codename}-backports";
 };
-Unattended-Upgrade::DevRelease "false";
-# Cleanup
+Unattended-Upgrade::DevRelease "auto";
+Unattended-Upgrade::AutoFixInterruptedDpkg "true";
+Unattended-Upgrade::MinimalSteps "true";
+Unattended-Upgrade::InstallOnShutdown "false";
+
+// Cleanup
 Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";
+// Equivalent to apt-get autoremove
 Unattended-Upgrade::Remove-Unused-Dependencies "true";
-# Reboot during the night
+
+// Reboot during the night/morning
 Unattended-Upgrade::Automatic-Reboot "true";
-Unattended-Upgrade::Automatic-Reboot-Time "02:00";
+Unattended-Upgrade::Automatic-Reboot-Time "06:00";
+
+// Enable verbose logging
+Unattended-Upgrade::Verbose "true";
+
+// Print debugging information both in unattended-upgrades and
+// in unattended-upgrade-shutdown
+Unattended-Upgrade::Debug "true";
 EOF
 
 # Enable Unattended Upgrades
 cat << EOF | sudo tee /etc/apt/apt.conf.d/20auto-upgrades
+// Enable the update/upgrade script (0=disable)
+APT::Periodic::Enable "1";
+// Do "apt-get update" automatically every n-days (0=disable)
 APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
 APT::Periodic::Download-Upgradeable-Packages "1";
 APT::Periodic::AutocleanInterval "7";
-APT::Periodic::Unattended-Upgrade "1";
 EOF
