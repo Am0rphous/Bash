b9a7e25c3c6b668ba4f5c42a614166c46d870f52
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2021-09-08 17:32:11 +0200

    Update UFW-CheatSheet.sh

--- Security/UFW-CheatSheet.sh
+++ Security/UFW-CheatSheet.sh
@@ -1,3 +1,4 @@
 #Various ufw commads
 
-Moved to https://github.com/Am0rphous/Cheat-Sheets/blob/main/UFW%20-%20Uncomplicated%20Firewall.md
+Moved to 
+https://github.com/Am0rphous/CheatSheets/blob/main/Linux/Network/UFW%20-%20Uncomplicated%20Firewall.md
