45e39f5806a211e8758a91fdd7ad891f2de5e1cd
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2026-03-30 16:29:11 +0200

    Optimize IP address scanning script
    Replaced the original scanning method with a more efficient approach using xargs and parallel to scan multiple IP addresses quickly.

--- network/scan-ip-adresses.sh
+++ network/scan-ip-adresses.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# ${1?"Usage: $0 ip subnet to scan. eg '192.168.1.'"}
-# bash scan-ip-adresses.sh 192.168.1
 
-subnet=192.168.1.
-for addr in `seq 130 190 `; do
-#   ( echo $subnet$addr)
-( ping -c 3 -t 5 $subnet$addr > /dev/null && echo $subnet$addr is Alive && nslookup $subnet$addr) &
-done 
+# Treg, bruker 1 sek per IP. Tar ca 4 minutter
+for a in {1..254}; do ip=10.0.1.$a; ping -W1 -4 -s0 -c1 "$ip" >/dev/null 2>&1 && echo "$ip" UP; wait; done
+
+# Heller: Scanner 254 hosts på noen sekunder. 8 b/packet = 8b x 254 = 2032b = 2kb. +Respons tot 4kb per /24 subnett. 3 subnett = 12 kb
+seq 1 254 | xargs -P200 -I% bash -c 'ip=10.0.1.%;   ping -W1 -4 -s0 -c1 "$ip" >/dev/null 2>&1 && echo "$ip UP"'
+seq 1 254 | xargs -P200 -n1 bash -c 'ip=10.0.1.$0;  ping -W1 -4 -s0 -c1 "$ip" >/dev/null 2>&1 && echo "$ip UP"'
+seq 1 254 | xargs -P200 -I{} bash -c 'ip=10.0.1.{}; ping -W1 -4 -s0 -c1 "$ip" >/dev/null 2>&1 && echo "$ip UP"'
+
+# Alternativ med 'parallel'. apt install parallel
+seq 1 254 | parallel -j200 'ip=10.0.1.{}; ping -W1 -4 -s0 -c1 "$ip" >/dev/null 2>&1 && echo "$ip UP"'
+
+# Flere subnett
+printf "%s\n" {10.0.1.,10.0.2.}{1..254} | xargs -P256 -I{} bash -c 'ip={}; ping -W1 -4 -s0 -c1 "$ip" >/dev/null 2>&1 && echo "$ip UP"'
